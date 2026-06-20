ce577cadf8e975066d949f0d8b26f76716380bd4
d1rshan <darshan.paccha@gmail.com>
2026-06-20 12:26:58 +0530

    chore: use neon serverless for db

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,7 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
 import * as schema from "./schema";
 
 // If DATABASE_URL is not set, don't crash at import time, but when queried.
-const queryClient = postgres(process.env.DATABASE_URL || "postgres://localhost:5432/tsuki");
-export const db = drizzle(queryClient, { schema });
+const sql = neon(process.env.DATABASE_URL || "postgres://localhost:5432/tsuki");
+export const db = drizzle({ client: sql, schema });
