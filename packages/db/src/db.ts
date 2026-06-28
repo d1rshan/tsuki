95831eef69d4d5d46fb13c5d8aad18fef78846c4
d1rshan <darshan.paccha@gmail.com>
2026-06-28 16:54:00 +0530

    fix build

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -4,5 +4,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
 // If DATABASE_URL is not set, don't crash at import time, but when queried.
-const sql = neon(process.env.DATABASE_URL || "postgres://localhost:5432/tsuki");
+const sql = neon(
+  process.env.DATABASE_URL || "postgres://postgres:postgres@localhost.com:5432/postgres",
+);
 export const db = drizzle({ client: sql, schema });
