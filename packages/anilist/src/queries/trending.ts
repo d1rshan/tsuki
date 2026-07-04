6d8f11e7c2a64db8c2fa2eff34f6a6a959abdcba
Darshan Paccha <darshan.paccha@gmail.com>
2026-07-04 19:36:27 +0530

    fix: dont show nsfw by default (#19)

--- packages/anilist/src/queries/trending.ts
+++ packages/anilist/src/queries/trending.ts
@@ -4,7 +4,7 @@ import type { AnilistMedia } from "../types";
 export const TRENDING_ANIME_QUERY = gql`
   query ($page: Int = 1, $perPage: Int = 50) {
     Page(page: $page, perPage: $perPage) {
-      media(type: ANIME, sort: TRENDING_DESC) {
+      media(type: ANIME, sort: TRENDING_DESC, isAdult: false) {
         id
         title {
           romaji
