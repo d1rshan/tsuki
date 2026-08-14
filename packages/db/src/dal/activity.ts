f8ff089eaf53ad3213b7085d8aecedb74b8ff9f1
d1rshan <darshan.paccha@gmail.com>
2026-08-14 15:35:41 +0530

    comment

--- packages/db/src/dal/activity.ts
+++ packages/db/src/dal/activity.ts
@@ -3,6 +3,10 @@ import { asc, eq } from "drizzle-orm";
 import { db } from "../db";
 import { progressActivity } from "../schema";
 
+/**
+ * Rows are stored by the progress trigger defined in
+ * `drizzle/0001_record_progress_activity.sql` whenever library progress increases.
+ */
 export const getProgressActivity = async (userId: string) => {
   return db
     .select({
